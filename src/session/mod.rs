3224764b80d832023e2d23f95b8d6f7f9e7d6484
SRugina <dev@srugina.com>
2019-09-24 23:21:16 +0100

    answer for doing many to many from discussion

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,8 +1,12 @@
 use crate::schema::sessions;
 use crate::schema::sessions_users;
+use crate::schema::users;
 use diesel::prelude::*;
 
 use crate::user::User;
+
+use crate::api::ApiResponse;
+use rocket::http::Status;
 
 #[table_name = "sessions"]
 #[derive(Identifiable, AsChangeset, Serialize, Deserialize, Queryable)]
@@ -23,4 +27,47 @@ pub struct Session {
 pub struct SessionUser {
     pub session_id: i32,
     pub user_id: i32,
+}
+
+/*
+Ok so let's say you have those three tables: users, groups and user_groups. Now you want to get all groups for a specific user.
+There are two variants doing that:
+
+    users::table.inner_join(user_groups::table.inner_join(groups::table)).filter(users::id.eq(user_id)).select(groups::all_columns).load::<Group>(conn)?;
+
+    let user = users::table.find(user_id).first::<User>(conn)?;
+    UserGroup::belonging_to(&user).inner_join(groups::table).select(groups::all_columns).load::<Group>(conn)?;
+
+    The first variant is only on query, but may not match your required data layout if you want to do that for all users,
+    The second variant allows to group the results for each user, using the build in associations api.
+
+For inserting (because this was also ask in the stackoverflow question): Yes this requires three separate inserts. We do not try to hide that because in the end it's a user choice how to handle for example data consistency in case of an failed insert there. (And yes, just using a transaction is a common choice) 
+*/
+
+impl Session {
+    pub fn get_for_user(user_id: i32, connection: &PgConnection) -> ApiResponse {
+        let sessions = users::table
+            .inner_join(sessions_users::table.inner_join(sessions::table))
+            .filter(users::id.eq(user_id))
+            .select(sessions::all_columns)
+            .load::<Session>(connection)
+            .unwrap();
+
+        let user = users::table
+            .find(user_id)
+            .first::<User>(connection)
+            .unwrap();
+        let alternate_sessions = SessionUser::belonging_to(&user)
+            .inner_join(sessions::table)
+            .select(sessions::all_columns)
+            .load::<Session>(connection)
+            .unwrap();
+        println!("Sessions: {:#?}", sessions);
+        println!("Alternate Sessions: {:#?}", alternate_sessions);
+
+        ApiResponse {
+            json: json!({ "sessions": sessions, "alt": alternate_sessions }),
+            status: Status::Ok,
+        }
+    }
 }
